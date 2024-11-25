b945b05fcb78da088b435f57c5108131a0de1f08
2Dcake61 <00001104088502sp@al.educacao.sp.gov.br>
2024-11-24 21:42:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'A que país pertencem as ilhas Canárias?',
+    'Espanha'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país africano tem a maior população?',
+    'Nigéria (190 milhões)'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que país você encontraria a cidade de Dresden?',
+    'Alemanha'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a maior ilha do mundo?',
+    'Groenlândia'
+)
